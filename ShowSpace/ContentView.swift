e8829bf8a8300484a3914b4ceb01bcbb479426fc
Apoorva Kanekal <apka8010@colorado.edu>
2023-10-25 22:47:01 -0600

    Initial Commit

---
+++ ShowSpace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShowSpace
+//
+//  Created by Apoorva Kanekal on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShowSpace/ShowSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowSpaceApp.swift
+//  ShowSpace
+//
+//  Created by Apoorva Kanekal on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ShowSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
